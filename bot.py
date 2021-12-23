a7bdc107ab1f18c149304a86c1dcb9a1d5d24ff6
Time <91857052+mkidcqpw@users.noreply.github.com>
2021-12-23 13:45:33 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="+")
+
+@client.event
+async def on_ready():
+    print("Bot is ready to go!!")
+
+@client.command(name="whois")
+async def whois(ctx,user:discord.Member=None):
+
+    if user==None:
+        user=ctx.author
+
+    rlist = []
+    for role in user.roles:
+      if role.name != "@everyone":
+        rlist.append(role.mention)
+
+    b = ", ".join(rlist)
+
+
+    embed = discord.Embed(colour=user.color,timestamp=ctx.message.created_at)
+
+    embed.set_author(name=f"User Info - {user}"),
+    embed.set_thumbnail(url=user.avatar_url),
+    embed.set_footer(text=f'Requested by - {ctx.author}',
+  icon_url=ctx.author.avatar_url)
+
+    embed.add_field(name='ID:',value=user.id,inline=False)
+    embed.add_field(name='Name:',value=user.display_name,inline=False)
+
+    embed.add_field(name='Created at:',value=user.created_at,inline=False)
+    embed.add_field(name='Joined at:',value=user.joined_at,inline=False)
+
+  
+ 
+    embed.add_field(name='Bot?',value=user.bot,inline=False)
+
+    embed.add_field(name=f'Roles:({len(rlist)})',value=''.join([b]),inline=False)
+    embed.add_field(name='Top Role:',value=user.top_role.mention,inline=False)
+
+    await ctx.send(embed=embed)
+  
+
+
+
+
+client.run("your token here")
